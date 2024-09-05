9c7705f9f3f8c0e1d60965acc7b78c2ddf3249c0
Domiragi <domiragi1408@gmail.com>
2024-09-05 20:03:30 +0700

    Update and add new aliases

--- .config/zsh/Base_configs/alias.zsh
+++ .config/zsh/Base_configs/alias.zsh
@@ -4,9 +4,10 @@
 setopt COMPLETEALIASES
 alias reshell='exec zsh'
 alias ls='ls --color=auto -Ahp --group-directories-first'
-alias l1='ls -1'
+alias l1='ls -1s'
 alias lo='ls -o'
-alias lt='lo -ct'
+alias lt='ls -oct' # Sort file by modification time
+alias ltr='ls -roct' # Sort file by modification time in reverse
 alias dir='dir --color=auto'
 alias grep='grep --colour=auto'
 alias egrep='egrep --colour=auto'
@@ -26,13 +27,16 @@ alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
 alias clear='clear -x'
+alias fdir='find . -type d -name' # Find dir in current directory
+alias ff='find . -type f -name' # Find file in current directory
+alias hgrep='fc -El 0 | grep' # Search command in history, display command number and time of execution
 # Print a simple colormap.
 alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+"\n"}; done'
 
 # YAY aliases
-alias yayi="yay -S"    # Install package
-alias yayil="yay -U"   # Install package from local file
-alias yayls="yay -Qe"  # List installed packages
-alias yayrm="yay -Rns" # Remove packaage with its settings and uneeded dependencies
-alias yaclr="yay -Scc" # Remove all old and unused cached packages
-alias yayinf="yay -Si" # Display info about a package in the repositories
+alias yayi="yay -S"     # Install package
+alias yayil="yay -U"    # Install package from local file
+alias yayls="yay -Qe"   # List installed packages
+alias yayrm="yay -Rns"  # Remove package with its settings and uneeded dependencies
+alias yayclr="yay -Scc" # Remove all old and unused cached packages
+alias yayinf="yay -Si"  # Display info about a package in the repositories
