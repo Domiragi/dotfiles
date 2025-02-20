c5d37e68598141ed00a731b30564ec1dc1372ac9
Domiragi <domiragi1408@gmail.com>
2025-02-21 04:02:56 +0700

    Add config for Firefox

---
+++ .config/firefox/user.js
@@ -0,0 +1,151 @@
+// Firefox User Preferences
+// User: Domiragi
+
+user_pref("accessibility.force_disabled", 1);
+user_pref("beacon.enabled", false);
+user_pref("browser.active_color", "#EE0000"); // Default color for active links (links currently being clicked on)
+user_pref("browser.active_color.dark", "#FF6666"); // Default color for active links after being opened once
+user_pref("browser.anchor_color", "#0000EE" ); // Default color for links
+user_pref("browser.anchor_color.dark", "#8C8CFF" ); // Default color for links after being opened once
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.editDialog.maxRecentFolders", 10);
+user_pref("browser.bookmarks.editDialog.showForNewBookmarks", true);
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("browser.cache.offline.enable", false); // Disable offline cache
+user_pref("browser.compactmode.show", true);
+user_pref("browser.contentanalysis.enabled", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.contentblocking.cryptomining.preferences.ui.enabled", true);
+user_pref("browser.contentblocking.fingerprinting.preferences.ui.enabled", true);
+user_pref("browser.contentblocking.report.hide_vpn_banner", true);
+user_pref("browser.ctrlTab.sortByRecentlyUsed", false);
+user_pref("browser.dataFeatureRecommendations.enabled", false);
+user_pref("browser.discovery.enabled", false); // Disable Firefox from recommending extensions
+user_pref("browser.display.show_focus_rings", true);
+user_pref("browser.download.alwaysOpenInSystemViewerContextMenuItem", true);
+user_pref("browser.download.alwaysOpenPanel", true);
+user_pref("browser.download.always_ask_before_handling_new_types", false);
+user_pref("browser.download.autohideButton", true);
+user_pref("browser.download.folderList", 2);
+user_pref("browser.download.forbid_open_with", false);
+user_pref("browser.download.open_pdf_attachments_inline", true); // Open PDF link in a new tab instead of instantly downloading
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.download.start_downloads_in_tmp_dir", false);
+user_pref("browser.download.useDownloadDir", false); // Ask everytime where to save file
+user_pref("browser.download.viewableInternally.typeWasRegistered.avif", true);
+user_pref("browser.download.viewableInternally.typeWasRegistered.webp", true);
+user_pref("browser.eme.ui.firstContentShown", true);
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+user_pref("browser.menu.showViewImageInfo", true);
+user_pref("browser.meta_refresh_when_inactive.disabled", true);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.navigation.requireUserInteraction", true);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false); // Disable "Recommend extensions as you browse"
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false); // Disable "Recommend features as you browse"
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.newtabWallpapers.highlightDismissed", true);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.enabled", true); // Set "New tab" to open Firefox Home (true) or blank page (false)
+user_pref("browser.privateWindowSeparation.enabled", false);
+user_pref("browser.protections_panel.infoMessage.seen", true);
+user_pref("browser.search.suggest.enabled.private", true);
+user_pref("browser.send_pings", false);
+user_pref("browser.sessionstore.interval", 60000); // Change how often Firefox write to disk/memory. Default: 15000
+user_pref("browser.sessionstore.resuming_after_os_restart", true);
+user_pref("browser.shopping.experience2023.ads.enabled", false);
+user_pref("browser.shopping.experience2023.ads.userEnabled", false);
+user_pref("browser.startup.blankWindow", true);
+user_pref("browser.startup.firstrunSkipsHomepage", true);
+user_pref("browser.startup.homepage", "about:home"); // Set "Homepage and new windows"
+user_pref("browser.startup.page", 1); // Set start page
+user_pref("browser.tabs.groups.enabled", true);
+user_pref("browser.tabs.loadBookmarksInBackground", true); // Opening bookmarks doesn't automatically switch to the new tab
+user_pref("browser.tabs.remote.warmup.maxTabs", 7); // Make more tabs opened in background pre-load for faster switching
+user_pref("browser.tabs.tabClipWidth", 999); // Make the close button only visible/enabled on currently active tab
+user_pref("browser.tabs.tabMinWidth", 1);
+user_pref("browser.taskbar.lists.enabled", false);
+user_pref("browser.taskbar.lists.frequent.enabled", false);
+user_pref("browser.taskbar.lists.maxListItemCount", 0);
+user_pref("browser.taskbar.lists.recent.enabled", false);
+user_pref("browser.taskbar.lists.tasks.enabled", false);
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.translations.neverTranslateLanguages", "en,vi");
+user_pref("browser.uidensity", 1); // Set density to Compact
+user_pref("browser.urlbar.maxRichResults", 15); // Set the max number of items shown up when using the URL bar. Default: 10
+user_pref("browser.urlbar.groupLabels.enabled", false);
+user_pref("browser.urlbar.shortcuts.bookmarks", false);
+user_pref("browser.urlbar.shortcuts.history", false);
+user_pref("browser.urlbar.shortcuts.tabs", false);
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false); // Stop Firefox from preemptively making connection to URL when enabling bookmark search in the URL bar
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.trimHttps", false);
+user_pref("browser.urlbar.trimURLs", false);
+user_pref("browser.urlbar.quicksuggest.enabled", false);
+user_pref("browser.vpn_promo.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", true);
+user_pref("datareporting.policy.dataSubmissionEnabled", true);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.cache.privateBrowsing.enabled", false);
+user_pref("dom.private-attribution.submission.enabled", false);
+user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("findbar.highlightAll", true);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.delay", 50);
+user_pref("full-screen-api.warning.timeout", 50);
+user_pref("general.autoScroll", true);
+user_pref("general.smoothScroll", true);
+user_pref("layout.css.backdrop-filter.enabled", true);// Enable applying graphical effects such as blurring or color shifting
+user_pref("layout.spellcheckDefault", 0);
+user_pref("layout.word_select.eat_space_to_next_word", false); // Do not select adjacent empty space when double clicking words
+user_pref("media.autoplay.default", 0); // Prevent autoplay for all media with sound
+user_pref("mousewheel.default.delta_multiplier_y", 300); // Default: 100
+user_pref("pdfjs.defaultZoomValue", "page-fit"); // Default PDF to open in Page-Fit
+user_pref("pdfjs.scrollModeOnLoad", 0); // Default PDF to use Vertical Scrolling
+user_pref("pdfjs.spreadModeOnLoad", 1); // Default PDF to open in Odd-Spread
+user_pref("privacy.donottrackheader.enabled", false);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("privacy.trackingprotection.pbmode.enabled", true); // Enable tracking protection in Private Browsing Mode
+user_pref("privacy.userContext.enabled", true);
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("signon.autofillForms", false); // Disable autofilling logins and passwords
+user_pref("signon.rememberSignons", false); // Never to ask to save logins and  passwords
+user_pref("slider.snapMultiplier", 0); // Disable effect: when dragging a scrollbar, if the mouse moves too far away then snap back to top
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // Enable userChrome.css
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.cachedClientID", "");
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.hybridContent.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.prompted", 2);
+user_pref("toolkit.telemetry.rejected", true);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.unifiedIsOptIn", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref(" ui.key.menuAccessKey", 0); // Remove underline of key shortcut for items when opeing the context menu
+user_pref("ui.key.menuAccessKeyFocuses", false); // Disable pressing Alt key opening the menu bar
+user_pref("webgl.disabled", true);
+user_pref("webgl.renderer-string-override", " ");
+user_pref("webgl.vendor-string-override", " ");
+user_pref("widget.non-native-theme.scrollbar.style", 2); // Set scrollbar style. Possible value: 0 (default, based on OS), 1-5
